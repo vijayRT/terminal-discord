a41c864b53c46ce6323cfe17eea69c958ec997b8
Vijay Rajasekar <vijayrajasekar1@gmail.com>
2019-12-08 02:19:45 -0500

    Rewrite post-install script for new config

--- post-install.js
+++ post-install.js
@@ -1,38 +1,23 @@
 const fs = require("fs");
-const configObject = require("./config.json");
-function generate_config() {
-  if (process.env.SUDO_UID !== undefined) {
-    console.log("Script is running as root user, aborting...");
-    console.log("Please see the Installation section of the README");
-    process.exit(0);
-  }
-  let homedir = process.env.HOME;
-  let configDirPath;
-  if (fs.existsSync(homedir + "/.config")) {
-    configDirPath = homedir + "/.config/terminal-discord";
-  } else {
-    configDirPath = homedir + "/.terminal-discord";
-  }
-  let configFilePath = configDirPath + "/config.json";
-  console.log("Does config directory exist: " + fs.existsSync(configDirPath));
-  console.log("Does config file exist: " + fs.existsSync(configFilePath));
-  if (!fs.existsSync(configDirPath)) {
-    console.log("\nGenerating config directory at " + configDirPath);
-    fs.mkdirSync(configDirPath);
-  }
-  if (!fs.existsSync(configFilePath)) {
-    console.log(
-      "\nGenerating default configuration file at " +
-        configFilePath +
-        " inside config directory.\n"
-    );
-    fs.writeFileSync(
-      configFilePath,
-      JSON.stringify(configObject, undefined, 4)
-    );
-  } else {
-    console.log("terminal-discord config already exists.");
-  }
-}
-
-generate_config();
+const path = require("path");
+const default_config = {
+  token: "",
+  max_name_length: null,
+  align: false,
+  separator: ":",
+  history_length: null,
+  default_guild: null,
+  default_channel: null,
+  mention_color: "#A52D00",
+  default_color: "#FFFFFF",
+  prompt: ">",
+  show_date: true,
+  show_time: true,
+  use_nickname: true,
+  select_count: 8,
+  color_support: true,
+  show_embeds: true,
+  repeat_name: true,
+  right_bound: false
+};
+fs.writeFileSync(path, JSON.stringify(default_config, undefined, 4));
